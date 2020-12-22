7ee21f589c48c7b0f42ecbe273783e9e7cb8d4f4
Andrew Huang <ahuang771@gmail.com>
2020-12-21 23:30:54 -0500

    Added inventory functions

---
+++ scanInventory.lua
@@ -0,0 +1,55 @@
+local SLOT_COUNT = 16
+local ERROR_NOT_FOUND = -1
+
+WANTED_ITEMS = {
+    ["minecraft:diamond"]=true,
+    ["minecraft:iron_ore"]=true,
+	["minecraft:gold_ore"]=true
+}
+
+function searchInventory(item_name)
+	for slot = 1, SLOT_COUNT, 1 do
+		local item_data = turtle.getItemDetail(slot)
+		if item_data["name"] == item_name then
+			return slot
+		end
+	end
+	return ERROR_NOT_FOUND
+end
+
+function filterInventory(filter_list)
+	for slot = 1, SLOT_COUNT, 1 do
+		local item_data = turtle.getItemDetail(slot)
+		if item_data then
+			if not filter_set[item_data["name"]] then
+				turtle.select(slot)
+				turtle.dropDown()
+			end
+		end
+	end
+end
+
+function printInventory()
+	for slot = 1, SLOT_COUNT, 1 do
+		local item_data = turtle.getItemDetail(slot)
+		print(item_data["name"])
+	end
+end
+
+function emptyInventory()
+	filterInventory({})
+end
+
+function test()
+	printInventory()
+	local result = searchInventory("minecraft:iron_ore")
+	if result then
+		print("Found iron ore")
+	end
+	filterInventory(WANTED_ITEMS)
+	printInventory()
+	emptyInventory()
+	printInventory()
+end
+
+test()
